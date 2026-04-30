8bc2ff43cdb5f30c0989845b5e0570819d86e1b3
77-GGG <qg24@mails.tsinghua.edu.cn>
2026-04-30 22:20:33 +0800

    Initial Commit

---
+++ MTeXTests/MTeXTests.swift
@@ -0,0 +1,17 @@
+//
+//  MTeXTests.swift
+//  MTeXTests
+//
+//  Created by qiguo on 4/30/26.
+//
+
+import Testing
+@testable import MTeX
+
+struct MTeXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
